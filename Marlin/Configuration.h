c2d898ed89411f74949c091c3106c092791f9122
Ryan Zellars <allted@gmail.com>
2018-01-22 18:07:39 -0800

    accel jerk stage 1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -549,7 +549,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 190, 190, 15, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 150, 150, 15, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -568,7 +568,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  5000    // E acceleration for retracts
+#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
@@ -579,8 +579,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 4.0
-#define DEFAULT_YJERK                 2.0
+#define DEFAULT_XJERK                  4.0
+#define DEFAULT_YJERK                  4.5
 #define DEFAULT_ZJERK                  0.2
 #define DEFAULT_EJERK                  5.0
 
