cd8a261d2e1bf048652704777d0af7ec68161565
Ryan Zellars <allted@gmail.com>
2018-04-08 14:55:44 -0700

    fix bad merge

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MINIRAMBO
+  #define MOTHERBOARD BOARD_RAMBO
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
