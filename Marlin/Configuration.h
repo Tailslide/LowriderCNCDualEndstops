5f4e6fd1a67fd2d13fa19ba7290e6a09a5f90f14
allted <allted@gmail.com>
2019-04-23 10:40:20 -0700

    Fix bad merge

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,13 +741,13 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                  5
-#define DEFAULT_YJERK                  5
-#define DEFAULT_ZJERK                  0.15
-#define DEFAULT_EJERK                  3.0
+#if DISABLED(JUNCTION_DEVIATION)
+  #define DEFAULT_XJERK 5.0
+  #define DEFAULT_YJERK 5.0
+  #define DEFAULT_ZJERK  0.15
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    3.0  // May be used by Linear Advance
 
 /**
  * S-Curve Acceleration
